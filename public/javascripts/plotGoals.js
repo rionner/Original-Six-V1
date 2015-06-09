ed1983b069a65da952c0e87e0d2732a507681661
Marion Huebner <mphuebner@gmail.com>
2015-06-09 10:21:55 -0500

    renders goals for & goals against pie chart

--- public/javascripts/plotGoals.js
+++ public/javascripts/plotGoals.js
@@ -1,25 +1,11 @@
 function plotGoals(data){
-
   var n = 2;
-
-  var goals = data.map(function(d, i){
-    if (!d.playoffs) {
-      var year = parseInt(d.year.substr(0, 4));
-      return { date: year, goals_for: d.team_goals, goals_against: d.opponent_goals };
-    } else {
-      return null;
-    }
-  }).filter(function(n){return n;});
-
-  var parseDate = d3.time.format("%m/%d/%Y").parse;
-
-  for (i = 0; i < data.length; i++) {
-            data[i].year = parseDate(data[i].Date);
-        }
-
+  var width = 500;
+  var height = 500;
+  var radius = height / 2;
 
   var colors = data.map(function(d, i) {
-      var arr = [];
+      var colorArray = [];
       var temp = {};
 
       temp.primary_color = d.primary_color;
@@ -35,41 +21,73 @@ function plotGoals(data){
           ("0" + parseInt(selectedColor[3],10).toString(16)).slice(-2) : '';
 
           temp[color] = selectedColor;
-          arr.push(selectedColor);
+          colorArray.push(selectedColor);
       }
-      return arr;
+      return colorArray;
   });
 
-
   var color = d3.scale.linear()
-    .domain([0, n - 1])
     .range(colors[0]);
 
+  var goalObjects = data.map(function(d, i){
+    if (!d.playoffs) {
+      var year = d.year.substr(0, 4);
+      return ({ label: year, value: [d.team_goals, d.opponent_goals] }) ;
+    }
+    else {
+      return null;
+    }
+  }).filter(function(n){return n;});
 
-  var width = 960,
-      height = 500,
-      radius = height / 2 - 10;
+  goalsPairs = goalObjects.map(function(d){
+    return d.value;
+  });
+
+  var goals = [].concat.apply([], goalsPairs).map(function(d){
+    return {value: d};
+  });
+
+  var svg = d3.select(".total-goals").append("svg")
+      .data([goals])
+          .attr("width", width)
+          .attr("height", height)
+      .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
   var arc = d3.svg.arc()
-      .innerRadius(radius - 40)
+      .innerRadius(radius - 60)
       .outerRadius(radius);
 
   var pie = d3.layout.pie()
-      .padAngle(.02);
+      .value(function(d, i) {
+          return d.value
+       })
+      .padAngle(0.01)
+      .sort(null);
 
-  var color = d3.scale.category10();
+  var arcs = svg.selectAll("g.slice")
+      .data(pie)
+      .enter()
+          .append("g")
+          .attr("class", "slice")
+            .attr("fill", function(d,i) {
+              return colors[0][i%2]; })
+          .append("g")
+          	.attr("class", "labels")
+          .append("g")
+          	.attr("class", "lines");
 
-  var svg = d3.select(".total-goals").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  var path = svg.selectAll("path")
-      .data(pie(goals))
-      .enter().append("path")
-      .style("fill", function(d, i) { return color(i); })
+  arcs.append("path")
       .attr("d", arc);
 
+  arcs.append("text")
+      .data(goalObjects)
+      // .attr("transform", function(d){
+      //   d.innerRadius = 0;
+      //   d.outerRadius = radius;
+      //   return "translate(" + arc.centroid(d) + ")";
+      // })
+      .attr("text-anchor", "middle")
+      .text(function(d, i) { return data[i].label; });
 
 };
